e0bfad670b8e81709aefa9f2aac5bf4eaec939a5
Domingo <986224568@qq.com>
2021-06-16 20:12:48 +0800

    [Domingo] feature test should_return_same_trade_id_from_repository_and_service_when_create_trade

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -15,6 +15,7 @@ class TradingServiceTest {
     @Mock
     AuditService auditService;
 
+    // task 1
     @Test
     void should_return_logNewTrade_time_when_execute_createTrade() {
         MockitoAnnotations.initMocks(this);
@@ -27,6 +28,7 @@ class TradingServiceTest {
         Mockito.verify(auditService, Mockito.times(1)).logNewTrade(trade);
     }
 
+    // task 2
     @Test
     void should_return_same_value_both_findTrade_and_findById_when_call_findTrade_and_findById() {
         MockitoAnnotations.initMocks(this);
@@ -43,15 +45,18 @@ class TradingServiceTest {
         assertEquals(tradeFromRepository.getReference(), tradeFromService.getReference());
     }
 
+    // task 3
     @Test
     void should_return_same_trade_id_from_repository_and_service_when_create_trade() {
         MockitoAnnotations.initMocks(this);
         // given
-
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        Trade trade = new Trade("", "");
         // when
-
+        Long idFromService = tradingService.createTrade(trade);
+        Long idFromRepository = tradeRepository.createTrade(trade);
         // then
-
+        assertEquals(idFromService, idFromRepository);
     }
 
 }
