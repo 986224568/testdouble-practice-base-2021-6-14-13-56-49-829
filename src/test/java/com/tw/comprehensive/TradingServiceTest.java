11c56b2881e2560b91ce4c188311a6a87d3a800b
Domingo <986224568@qq.com>
2021-06-16 18:39:14 +0800

    [Domingo] add new test should_return_logNewTrade_time_when_execute_createTrade

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,27 @@
 package com.tw.comprehensive;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradingServiceTest {
+    @Mock
+    TradeRepository tradeRepository;
+
+    @Mock
+    AuditService auditService;
+
+    @Test
+    void should_return_logNewTrade_time_when_execute_createTrade() {
+        // given
+
+        // when
+
+        // then
+
+    }
 
 }
