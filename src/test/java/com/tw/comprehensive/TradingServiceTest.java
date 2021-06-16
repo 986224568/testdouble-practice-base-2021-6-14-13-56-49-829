dcffc05e3d652dd5ccfeebffa336e5171d06405a
Domingo <986224568@qq.com>
2021-06-16 18:42:45 +0800

    [Domingo] feature test should_return_logNewTrade_time_when_execute_createTrade

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -16,12 +16,14 @@ class TradingServiceTest {
 
     @Test
     void should_return_logNewTrade_time_when_execute_createTrade() {
+        MockitoAnnotations.initMocks(this);
         // given
-
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+        Trade trade = new Trade("", "");
         // when
-
+        tradingService.createTrade(trade);
         // then
-
+        Mockito.verify(auditService, Mockito.times(1)).logNewTrade(trade);
     }
 
 }
